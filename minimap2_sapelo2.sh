5d0fb9d46f60aec96dbe35b9ea2509d8bf8f6cea
Ruijie Xu <rx32940@sapelo2-sub2.ecompute>
2019-08-23 12:30:42 -0400

    mapping + samtools

--- minimap2_sapelo2.sh
+++ minimap2_sapelo2.sh
@@ -12,6 +12,7 @@
 cd $PBS_O_WORKDIR
 
 ml minimap2/2.13-foss-2016b
+module load SAMtools/1.9-foss-2016b
 
 data_path="/scratch/rx32940/minion_blood_simulation/data"
 output_path="/scratch/rx32940/minion_blood_simulation/test_runs/map"
@@ -19,3 +20,14 @@ output_path="/scratch/rx32940/minion_blood_simulation/test_runs/map"
 # for nanopore mapping
 minimap2 -ax map-ont $data_path/Lepto_ref/Leptospira_interrogans_Copenhageni.fa $data_path/demultiplex2/barcode01/barcode01.fastq > $output_path/barcode01.sam
 
+# sam to bam 
+samtools view -b -o $output_path/barcode01.bam $output_path/barcode01.sam
+
+# sort alignment by leftmost chromosomal coordinates
+samtools sort -o $output_path/barcode01_sort.bam $output_path/barcode01.bam
+
+# indexing the bam file
+samtools index $output_path/barcode01_sort.bam
+
+# mapping stats
+samtools flagstat $output_path/barcode01_sort.bam
